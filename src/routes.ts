00ea1fe65e340781ac19bb0a60bc96ace8128584
henrique.hartwig <henrique2805@gmail.com>
2024-10-21 14:54:50 -0300

    feat: create port and adapters of Product Category

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { OrderController } from './infrastructure/http/OrderController';
+import { ProductCategoryController } from './infrastructure/http/ProductCategoryController';
 
 const router = Router();
 const orderController = new OrderController();
 
 router.post('/orders', orderController.createOrder);
 router.get('/orders/:id', orderController.getOrderById);
+
+const productCategoryController = new ProductCategoryController();
+router.post('/product-categories', productCategoryController.createProductCategory);
+router.get('/product-categories', productCategoryController.getAllProductCategories);
+router.get('/product-categories/:id', productCategoryController.getProductCategoryById);
 
 export { router as routes };
