d9ac39c477e04f5b17e71176efe1311dec1010ff
henrique.hartwig <henrique2805@gmail.com>
2024-10-21 18:28:13 -0300

    feat: add seed script

--- src/infrastructure/database/prisma/seed.ts
+++ src/infrastructure/database/prisma/seed.ts
@@ -16,11 +16,14 @@ async function main() {
 
   const products = await prisma.product.createMany({
     data: [
-      { name: 'X-Tudo', description: 'O clássico que você precisa!', price: 19.99 },
-      { name: 'X-Burger', description: 'O clássico que você precisa!', price: 19.99 },
-      { name: 'X-Salada', description: 'O clássico que você precisa!', price: 19.99 },
-      { name: 'X-Bacon', description: 'O clássico que você precisa!', price: 19.99 },
-      { name: 'X-Egg', description: 'O clássico que você precisa!', price: 19.99 },
+      { name: 'X-Tudo', description: 'O clássico completo!', price: 34.99, categoryId: 1 },
+      { name: 'X-Burger', description: 'O clássico essencial!', price: 22.99, categoryId: 1 },
+      { name: 'X-Salada', description: 'O clássico saudável!', price: 24.99, categoryId: 1 },
+      { name: 'Batata frita', description: 'Todo mundo gosta!', price: 12.99, categoryId: 2 },
+      { name: 'Onion rings', description: 'Cebolas gourmet!', price: 10.99, categoryId: 2 },
+      { name: 'Coca-Cola', description: 'Refrigerante clássico!', price: 7.99, categoryId: 3 },
+      { name: 'Fanta Laranja', description: 'Refrigerante mais divertido!', price: 7.99, categoryId: 3 },
+      { name: 'Milkshake', description: 'Delicioso milkshake!', price: 14.99, categoryId: 4 },
     ],
   })
 
